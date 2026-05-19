63a3981ba9be151e5bea86a745e176d3e823d059
Jan K <jan.kostulski@web.de>
2026-05-19 18:04:12 +0200

    chore: add colors vocab

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Anki Scribe - Service Worker
 // Provides offline support via caching
 
-const CACHE_NAME = 'anki-scribe-v20';
+const CACHE_NAME = 'anki-scribe-v22';
 
 // App shell files to pre-cache on install
 const APP_SHELL = [
